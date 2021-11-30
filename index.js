6ebe78920c08335fd4e269381197b531be590e37
Halil Bulut <halill.bulutt.1@gmailcom>
2021-11-30 22:20:08 +0100

    some changes

--- index.js
+++ index.js
@@ -7,15 +7,16 @@ const forSaleLis = document.querySelectorAll(".forSaleDropDown li");
 const forRent = document.querySelector(".forRent");
 const forRentDropDown = document.querySelector(".forRentDropDown");
 const forRentLis = document.querySelectorAll(".forRentDropDown li");
+const body = document.querySelector("body");
 
-function listOpener(dropDown,lis,condition){
+function listOpener(dropDown, lis, condition) {
     if (!condition) {
         dropDown.style.opacity = "1";
-        lis.forEach(element => element.style.height = "20px")
+        lis.forEach((element) => (element.style.height = "20px"));
         condition = !condition;
     } else {
         dropDown.style.opacity = "0";
-        lis.forEach(element => element.style.height = "0px")
+        lis.forEach((element) => (element.style.height = "0px"));
         condition = !condition;
     }
 }
@@ -23,40 +24,37 @@ function listOpener(dropDown,lis,condition){
 let isSaleOpen = false;
 
 forSale.addEventListener("click", () => {
-
     if (!isSaleOpen) {
         forSaleDropDown.style.opacity = "1";
-        forSaleLis.forEach(element => element.style.height = "20px")
+        forSaleLis.forEach((element) => (element.style.height = "20px"));
         isSaleOpen = !isSaleOpen;
+        if (isRentOpen) {
+            forRentDropDown.style.opacity = "0";
+            forRentLis.forEach((element) => (element.style.height = "0px"));
+            isRentOpen = !isRentOpen;
+        }
     } else {
         forSaleDropDown.style.opacity = "0";
-        // forRentLis.map((item) => {
-        //     item.style.height = "0px";
-        // });
-        forSaleLis.forEach(element => element.style.height = "0px")
+        forSaleLis.forEach((element) => (element.style.height = "0px"));
         isSaleOpen = !isSaleOpen;
     }
-    
 });
 
 let isRentOpen = false;
 
 forRent.addEventListener("click", () => {
-
     if (!isRentOpen) {
         forRentDropDown.style.opacity = "1";
-        // forRentLis.map((item) => {
-        //     // item.style.height = "20px";
-        //     console.log(item)
-        // });
-        forRentLis.forEach(element => element.style.height = "20px")
+        forRentLis.forEach((element) => (element.style.height = "20px"));
         isRentOpen = !isRentOpen;
+        if (isSaleOpen) {
+            forSaleDropDown.style.opacity = "0";
+            forSaleLis.forEach((element) => (element.style.height = "0px"));
+            isSaleOpen = !isSaleOpen;
+        }
     } else {
         forRentDropDown.style.opacity = "0";
-        // forRentLis.map((item) => {
-        //     item.style.height = "0px";
-        // });
-        forRentLis.forEach(element => element.style.height = "0px")
+        forRentLis.forEach((element) => (element.style.height = "0px"));
         isRentOpen = !isRentOpen;
     }
 });
